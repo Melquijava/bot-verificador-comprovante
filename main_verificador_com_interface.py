ed50933b586992092ad7d70f7bf09a72be518e52
Melquijava <melqueribeiroamorim@gmail.com>
2025-06-05 23:06:13 -0300

    � Atualização real do verificador com OCR e tratamento de PDF

--- main_verificador_com_interface.py
+++ main_verificador_com_interface.py
@@ -23,7 +23,7 @@ VALOR_REGEX = r"R\$\s?([0-9]+,[0-9]{2})"
 CATEGORIA_NOME = "⇓━━━━━━━━  Atendimento ━━━━━━━━⇓"
 CANAL_INICIAL = "📥│envio-comprovante"
 
-# Garante que as pastas existem somente no ambiente local
+# Garante que as pastas existem
 os.makedirs("images", exist_ok=True)
 os.makedirs("pdf_temp", exist_ok=True)
 
